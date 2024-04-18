f3d1576271103d97b5f3d0e1af44f0f5f495ab6e
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-18 22:31:18 +0900

    コメント追加

--- src/TestUserDAO.java
+++ src/TestUserDAO.java
@@ -11,33 +11,52 @@ public class TestUserDAO {
 	
 	//voidは戻り値がないメソッドで指定する特別な型
 	public void select(String name, String password) {
+		
+		//DBへの接続準備、DBと会話するためのコード、これでログインできる
 		DBConnector db = new DBConnector();
 		Connection con = db.getConnection();
-	
+		
+		/*test_tableに入っているデータ、user_name=? password=?に入る2つの条件を満たしたデータがsqlに代入される。
+		 ?はプレースホルダと言って、その都度違うデータを入れて使用する。
+		［例］user_name="taro" password="123"とした場合は太朗と１２３しかデータを抽出することができなくなる。*/
 		String sql ="select * from test_table where user_name=? and password=?";
-	
+		
+		//tryの中でエラーが発生した場合に、catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる
 		try {
+			
+			//PreparedStatementとは DB まで運んでくれる箱
 			PreparedStatement ps = con.prepareStatement(sql);
 			
+			//データベースの中に入るデータ
 			ps.setString(1, name);
 			ps.setString(2, password);
 			
+			//executeQuery();は実行メソッドで、必ず ResultSetが返ってくる
 			ResultSet rs = ps.executeQuery();
-		
+			
+			//ここでは2つのことをしている
+			//下に1行ずらすこと
+			//データが存在していれば戻り値を true で返す・存在しなければ falseで返す
 			if (rs.next()) {
 				System.out.println(rs.getString("user_name"));
 				System.out.println(rs.getString("password"));
 				
 			}
-			
+		//catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる	
+		//SQLException（クラスが見つからない場合の例外）
 		} catch (SQLException e) {
 			e.printStackTrace();
 			
 		}
-		
+		//tryの中でエラーが発生した場合に、catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる
 		try {
+			
+			//データベースとの接続をクローズ
+			//これをしないとデータベースを接続したまま作業が実行されてしまってメモリに負荷がかかる
 			con.close();
 			
+		//catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる	
+		//SQLException（クラスが見つからない場合の例外）
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -51,12 +70,10 @@ public class TestUserDAO {
 		DBConnector db = new DBConnector();
 		Connection con = db.getConnection();
 
-		/*test_tableに入っているデータ、user_name=? password=?に入る2つの条件を満たしたデータがsqlに代入される。
-		 ?はプレースホルダと言って、その都度違うデータを入れて使用する。
-		［例］user_name="taro" password="123"とした場合は太朗と１２３しかデータを抽出することができなくなる。*/
+		//test_tableに入っているデータがsqlに代入される
 		String sql = "select * from test_table";
 
-		//try.catchはjavaの例外処理のための構文
+		//tryの中でエラーが発生した場合に、catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる
 		try {
 
 			//PreparedStatementとはDBまで運んでくれる箱のこと
@@ -72,24 +89,21 @@ public class TestUserDAO {
 				
 			}
 
-			/*tryの中でエラーが発生した場合、catchが受け取り
-			 printStackTraceでエラーに至る履歴を表示してくれる*/
+			//catchが受け取りprintStackTraceでエラーに至る履歴を表示してくれる
 			//SQLException（クラスが見つからない場合の例外）
 			} catch (SQLException e) {
 				e.printStackTrace();
 		}
 
-		//try.catchはjavaの例外処理のための構文
+		//tryの中でエラーが発生した場合に、catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる
 		try {
 
 			//データベースとの接続をクローズ
 			//これをしないとデータベースを接続したまま作業が実行されてしまってメモリに負荷がかかる
 			con.close();
 
-			/*tryの中でエラーが発生した場合、catchが受け取り
-			 printStackTraceでエラーに至る履歴を表示してくれる*/
-			//SQLException（クラスが見つからない場合の例外）
-			
+		//catchが受け取りprintStackTraceでエラーに至る履歴を表示してくれる
+		//SQLException（クラスが見つからない場合の例外）
 		} catch (SQLException e) {
 			e.printStackTrace();
 			
@@ -97,61 +111,96 @@ public class TestUserDAO {
 	
 	}
 	
+	//voidは戻り値がないメソッドで指定する特別な型
 	public void selectByName(String name) {
+		
+		//DBへの接続準備、DBと会話するためのコード、これでログインできる
 		DBConnector db = new DBConnector();
 		Connection con = db.getConnection();
 		
+		//test_tableに入っているデータuser_name=? の条件を満たしたデータがsqlに代入される
 		String sql ="select * from test_table where user_name=?";
 		
+		//tryの中でエラーが発生した場合に、catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる
 		try {
+			
+			//PreparedStatementとは DB まで運んでくれる箱
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setString(1, name);
 			
+			//executeQuery();は実行メソッドで、必ず ResultSetが返ってくる
 			ResultSet rs = ps.executeQuery();
 			
+			//while(rs.next())はカーソルを1行ずつ実行していきデータがなくなったら実行を終了する
 			while (rs.next()) {
 				System.out.println(rs.getString("user_name"));
 				System.out.println(rs.getString("password"));
 				
 			}
-			
+		
+		//catchが受け取りprintStackTraceでエラーに至る履歴を表示してくれる
+		//SQLException（クラスが見つからない場合の例外）
 		} catch (SQLException e) {
 			e.printStackTrace();
 			
 		}
 		
+		//tryの中でエラーが発生した場合に、catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる
 		try {
+			
+			//データベースとの接続をクローズ
+			//これをしないとデータベースを接続したまま作業が実行されてしまってメモリに負荷がかかる
 			con.close();
 			
+		//catchが受け取りprintStackTraceでエラーに至る履歴を表示してくれる
+		//SQLException（クラスが見つからない場合の例外）	
 		} catch (SQLException e) {
 			e.printStackTrace();
 			
 		}
 	}
 	
+	//voidは戻り値がないメソッドで指定する特別な型
 	public void selectByPassword(String password) {
+		
+		//DBへの接続準備、DBと会話するためのコード、これでログインできる
 		DBConnector db = new DBConnector();
 		Connection con = db.getConnection();
 		
+		//test_tableに入っているデータ password=?に入る条件を満たしたデータがsqlに代入される
 		String sql ="select * from test_table where password=?";
 		
+		//tryの中でエラーが発生した場合に、catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる
 		try {
+			
+			//PreparedStatementとは DB まで運んでくれる箱
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setString (1, password);
+			
+			//executeQuery();は実行メソッドで、必ず ResultSetが返ってくる
 			ResultSet rs = ps.executeQuery();
 			
+			//while(rs.next())はカーソルを1行ずつ実行していきデータがなくなったら実行を終了する
 			while (rs.next()) {
 				System.out.println(rs.getString("user_name"));
 				System.out.println(rs.getString("password"));
 			}
-			
+		
+		//catchが受け取りprintStackTraceでエラーに至る履歴を表示してくれる
+		//SQLException（クラスが見つからない場合の例外）	
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		
+		//tryの中でエラーが発生した場合に、catchが受け取り、printStackTraceでエラーに至る履歴を表示してくれる
 		try {
-			con.close() ;
 			
+			//データベースとの接続をクローズ
+			//これをしないとデータベースを接続したまま作業が実行されてしまってメモリに負荷がかかる
+			con.close() ;
+		
+		//catchが受け取りprintStackTraceでエラーに至る履歴を表示してくれる
+		//SQLException（クラスが見つからない場合の例外）
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
