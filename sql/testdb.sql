ebe5496eaa08d815aa5ddc36341ae2adfecb7a1e
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-19 22:39:50 +0900

    演習課題14まで、エラー出る

--- sql/testdb.sql
+++ sql/testdb.sql
@@ -10,3 +10,4 @@ password varchar(255));
 insert into test_table values(1, "taro", "123");
 insert into test_table values(2, "jiro", "456");
 insert into test_table values(3, "hanako", "789");
+insert into test_table values(4, "shiro", "012");
